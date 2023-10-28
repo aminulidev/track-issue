5b41bc35eacd015dceb48c6723e2c72417d1075e
aminulds <aminulds61@gmail.com>
2023-10-28 13:51:46 +0600

    active link added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,33 @@
+"use client";
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BsSubtract } from 'react-icons/bs';
+import classNames from 'classnames';
+
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+    const currentPath = usePathname();
+    
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
     ]
+
     return (
         <nav className='flex items-center px-5 space-x-6 h-14 border-b'>
             <Link href="/" className=' text-indigo-500'><BsSubtract className="text-3xl" /></Link>
             <ul className='flex space-x-6'>
                 {links.map(link => 
-                    <li key={link.label}><Link href={link.href} className='text-zinc-500 font-semibold hover:text-indigo-500 transition-colors'>{link.label}</Link></li>
+                    <li key={link.label}><Link href={link.href} className={classNames({
+                        'text-indigo-500': link.href === currentPath,
+                        'text-zinc-500': link.href !== currentPath,
+                        'hover:text-indigo-500 transition-colors': true
+                    })}>{link.label}</Link></li>
                 )}
             </ul>
         </nav>
