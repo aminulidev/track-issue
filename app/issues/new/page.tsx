3e57202d2db174d6e7b08a99d00c2bda7f8aeadd
aminulds <aminulds61@gmail.com>
2023-10-28 20:49:17 +0600

    issue form submit on api

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,32 @@
+"use client";
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import React from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form'
 
+interface IssueForm {
+    title: string;
+    description: string;
+}
 const NewIssuePage = () => {
+    const router = useRouter();
+    const { register, handleSubmit } = useForm<IssueForm>();
+
     return (
         <div>
-            <div className='bg-slate-100 p-3 sm:p-5 rounded-sm max-w-xl mx-auto space-y-3'>
-                <Input placeholder='Title' />
-                <Textarea />
+            <form
+                className='bg-slate-100 p-3 sm:p-5 rounded-sm max-w-xl mx-auto space-y-3'
+                onSubmit={handleSubmit( async (data) => {
+                    await axios.post('/api/issues', data);
+                    router.push("/issues");
+                })}
+            >
+                <Input placeholder='Title' {...register("title")} />
+                <Textarea {...register("description")} />
                 <Button type='submit'>Submit New Issue</Button>
-            </div>
+            </form>
         </div>
     )
 }
