1fe3d2d6207e6fc61533c9d8b6ec42e7e93506f0
aminulds <aminulds61@gmail.com>
2023-11-07 11:04:27 +0600

    issue filter done

--- app/issues/_components/SelectIssueFilter.tsx
+++ app/issues/_components/SelectIssueFilter.tsx
@@ -9,10 +9,10 @@ import {
 import { Status } from "@prisma/client";
 import { useRouter } from "next/navigation";
 
-const statuses: { label: string, value?: Status }[] = [
-    { label: 'All' },
+const statuses: { label: string, value: Status | string }[] = [
+    { label: 'All', value: 'ALL' },
     { label: 'Open', value: 'OPEN' },
-    { label: 'In Progeress', value: 'IN_PROGRESS' },
+    { label: 'In Progress', value: 'IN_PROGRESS' },
     { label: 'Close', value: 'CLOSE' },
 ];
 
@@ -21,15 +21,15 @@ const SelectIssueFilter = () => {
 
     return (
         <Select onValueChange={(status) => {
-            const query = status ? `?status=${status}` : '';
+            const query = status === 'ALL' ? '' : `?status=${status}`;
             router.push('/issues' + query);
         }}>
-            <SelectTrigger className="w-[180px] [&_span]:flex [&_span]:items-center [&_span]:gap-2 ">
+            <SelectTrigger className="w-[180px] [&_span]:flex [&_span]:items-center [&_span]:gap-2">
                 <SelectValue placeholder="Filter by status" />
             </SelectTrigger>
             <SelectContent>
                 {statuses.map((status) => (
-                    <SelectItem key={status.value} value={status.value || ' '}>{status.label}</SelectItem>
+                    <SelectItem key={status.value} value={status.value || 'ALL'} placeholder="Filter by status">{status.label}</SelectItem>
                 ))}
             </SelectContent>
         </Select>
